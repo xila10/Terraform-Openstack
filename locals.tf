560fd161e024122570641da97bd2d077b3007484
Interested-Person-2000 <nn.vision@protonmail.com>
2026-03-11 15:05:35 +0100

    Delete locals.tf

--- locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  float_ip_pool = "external-net"
-  debian_13_id = "67e184fb-1fb2-4ef8-8c04-a570a2f098c0"
-  ubuntu_2204_id = "451decd7-4e93-4e21-9f87-65a3031b9792"
-  ubuntu_2404_id = "c1ec8a28-01e2-417d-91d7-174837d967cc"
-  rocky_9_id = "baa051f6-eb5c-4395-bd6c-5fa636ddb0f7"
-}
