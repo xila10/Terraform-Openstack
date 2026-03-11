6f2f45b27ce1c1b09146068821f406e40fe715c7
Interested-Person-2000 <nn.vision@protonmail.com>
2026-03-11 15:30:24 +0100

    Add HTTP and HTTPS rules for Nextcloud security group
    Added security group rules for HTTP and HTTPS access for Nextcloud users.

--- terraform code/security-groups.tf
+++ terraform code/security-groups.tf
@@ -26,12 +26,23 @@ resource "openstack_networking_secgroup_rule_v2" "ssh_from_bastion" {
   security_group_id = openstack_networking_secgroup_v2.cobra_sg.id
 }
 
-# HTTP/HTTPS for Nextcloud users
-resource "openstack_networking_secgroup_rule_v2" "http_https" {
+# HTTP for Nextcloud users
+resource "openstack_networking_secgroup_rule_v2" "http" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.cobra_sg.id
+}
+
+# HTTPS for Nextcloud users
+resource "openstack_networking_secgroup_rule_v2" "https" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
   port_range_max    = 443
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.cobra_sg.id
